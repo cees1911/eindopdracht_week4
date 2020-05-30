ed6c868c1ddb02b8ec908a3e4e96cc75c2e8f0b6
Cees Klap <ceesklap@gmail.com>
2020-05-30 13:49:23 +0200

    test file voor het testen van requiement 2

---
+++ guessLetter.test.js
@@ -0,0 +1,22 @@
+/* guessletter is het motortje dat de output naar de DOM regelt. 
+   De function wordt aangeroepen oor op de guess button te drukken  De functie test 
+            1 is het spel nog niet afgelopen                  
+            2 is er geen spatie ingevoerd                  
+            3 of de ingevoerde letter in het word zit                    
+   bij false het verhoogt de tries ++ en veranderd 
+   in de DOM het aantal levens  Maak een array met alle ingevoerde letters.  
+   Roept de functie theWord aan deze maakt deze vult de goed geraden letter  
+   in de DOM op de juiste plaats de niet geraden letters krijgt een "_"  
+   Roept de functie theLetter aan en deze plaats de fout geraden letter  */
+
+
+
+  const komtLetterVoorWoord = require("./script.js");
+
+  test("Komt letter voor in woord", function () {
+      const woord = "moestuin";
+      const ingevoerdeLetter = "r";
+      const output = woord.includes(ingevoerdeLetter);
+      expect(output).toBe(true);
+
+  }) 
